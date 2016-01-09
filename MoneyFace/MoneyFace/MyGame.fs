a8a83e47c7c917e247a4108ffc4ac7615190e81d
hakelimopu <hakelimopu@gmail.com>
2016-01-08 23:35:59 -0600

    v0.2

---
+++ MoneyFace/MoneyFace/MyGame.fs
@@ -0,0 +1,39 @@
+﻿module MyGame
+
+open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Graphics
+open Microsoft.Xna.Framework.Content
+
+type TextureId = 
+    | Avatar
+    | Dollar
+
+let loadTextures (contentManager:ContentManager) =
+    [(Avatar,"avatar"); (Dollar, "dollar")]
+    |> Seq.map(fun (id, filename) -> (id, contentManager.Load<Texture2D>(filename)))
+    |> Map.ofSeq
+
+let drawGame delta (textures:Map<TextureId,Texture2D>) (spriteBatch: SpriteBatch) = 
+    spriteBatch.Draw(textures.[Avatar],new Rectangle(0,0,32,32),Color.White)
+    spriteBatch.Draw(textures.[Dollar],new Rectangle(32,0,32,32),Color.White)
+
+type MyGame<'textureKey when 'textureKey: comparison> (textureLoader: ContentManager -> Map<'textureKey, Texture2D>, gameRenderer : GameTime -> Map<'textureKey,Texture2D> -> SpriteBatch -> unit) as this=
+    inherit Game()
+    do
+        this.Content.RootDirectory <- "Content"
+    let graphics = new GraphicsDeviceManager(this)
+    let mutable spriteBatch: SpriteBatch = null
+    let mutable textures: Map<'textureKey, Texture2D> = Map.empty
+    override this.Initialize() =
+        spriteBatch <- new SpriteBatch(this.GraphicsDevice)
+        base.Initialize()
+    override this.LoadContent() = 
+        textures <- this.Content |> textureLoader 
+    override this.Update delta =
+        ()
+    override this.Draw delta =
+        Color.Black 
+        |> this.GraphicsDevice.Clear
+        spriteBatch.Begin()
+        gameRenderer delta textures spriteBatch
+        spriteBatch.End()
--- MoneyFace/MoneyFace/Program.fs
+++ MoneyFace/MoneyFace/Program.fs
@@ -2,42 +2,11 @@
 open BoardState
 open System.Threading
 
-let initializeConsole () =
-    Console.Title <- "Money Face $ :)"
-    Console.WindowWidth <- 40
-    Console.WindowHeight <- 25
-    Console.BufferWidth <- Console.WindowWidth
-    Console.BufferHeight <- Console.WindowHeight
-    Console.CursorVisible <- false
-
-let renderBoardState (boardState:BoardState) =
-    Console.Clear()
-    let playerX, playerY = boardState.Player
-    Console.CursorLeft <- playerX
-    Console.CursorTop <- playerY
-    Console.Write ("@")
-    let dollarX, dollarY = boardState.Dollar
-    Console.CursorLeft <- dollarX
-    Console.CursorTop <- dollarY
-    Console.Write ("$")
-    Console.CursorLeft <- boardColumns
-    Console.CursorTop <- 0
-    Console.Write (boardState.Score |> sprintf "%i")
-    Thread.Sleep(50)
-    boardState
-
 type GameState =
     | Play
     | Quit
 
 let mutable gameState: GameState = Play
-
-let readKey () =
-    if Console.KeyAvailable then
-        Console.ReadKey(true).Key
-        |> Some
-    else
-        None
 
 let eatDollar boardState = 
     if boardState.Player = boardState.Dollar then
@@ -45,36 +14,8 @@ let eatDollar boardState =
     else
         boardState
 
-let processInput boardState =
-    let deltaX, deltaY =
-        match readKey() with
-        | Some ConsoleKey.UpArrow -> (0, -1)
-        | Some ConsoleKey.DownArrow -> (0, 1)
-        | Some ConsoleKey.LeftArrow -> (-1 ,0)
-        | Some ConsoleKey.RightArrow -> (1 ,0)
-        | _ -> (0,0)
-    let newPlayer = ((boardState.Player |> fst) + deltaX, (boardState.Player |> snd) + deltaY)
-    {boardState with Player = newPlayer}
-    |> eatDollar
-
 [<EntryPoint>]
 let main argv = 
-    initializeConsole()
-    while gameState <> Quit do
-        loadBoardState()
-        |> renderBoardState
-        |> processInput
-        |> saveBoardState
+    let game = new MyGame.MyGame<MyGame.TextureId>(MyGame.loadTextures, MyGame.drawGame)
+    game.Run()
     0
-
-//Avatar: smiley face \u0002
-//Time attack: 60 second timer
-//Goal: get points, $
-//Wrinkle: manage mood. 
-//Heart \u0003 will put you in a good mood
-//Good mood means you move faster
-//Bad mood means you move slower
-//Hearts cost $
-//Must rest or you will also slow down.
-//Rest is represented by a Z
-
