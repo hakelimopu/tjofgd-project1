391051d54a5dde0ff313ee7c534f843110cac504
hakelimopu <hakelimopu@gmail.com>
2016-01-20 22:15:15 -0600

    v0.17

---
+++ MoneyFace/MoneyFace/GameJoltApi.fs
@@ -0,0 +1,31 @@
+﻿module GameJoltApi
+
+open System.Security.Cryptography
+
+let private gjGameId = 120704
+let private gjPrivateKey = "8fa287db58661cb6c498ca6b39cd8f58"
+
+//BOOO! MUTABLE!
+let mutable private gjUserName = ""
+let mutable private gjUserToken = ""
+
+let setUser userName userToken =
+    gjUserName <- userName
+    gjUserToken <- userToken
+
+let authenticateUser () =
+    let baseUrl = sprintf "http://gamejolt.com/api/game/v1/users/auth/?game_id=%i&username=%s&user_token=%s" gjGameId gjUserName gjUserToken
+    let hashMe = sprintf "%s%s" baseUrl gjPrivateKey
+    let md5 = MD5.Create()
+    let bytes = md5.ComputeHash(System.Text.Encoding.UTF8.GetBytes(hashMe))
+    let signature =
+        bytes
+        |> Seq.fold (fun s b -> s+System.String.Format("{0:x2}",b)) System.String.Empty
+    let finalUrl = sprintf "%s&signature=%s" baseUrl signature
+    let request = System.Net.WebRequest.Create(finalUrl)
+    request.Method <- "GET"
+    let response = request.GetResponse()
+    use stream = response.GetResponseStream()
+    use reader = new System.IO.StreamReader(stream)
+    let result = reader.ReadToEnd()
+    result
--- MoneyFace/MoneyFace/Program.fs
+++ MoneyFace/MoneyFace/Program.fs
@@ -6,6 +6,8 @@ open Microsoft.Xna.Framework.Audio
 
 [<EntryPoint>]
 let main argv = 
+    GameJoltApi.setUser argv.[0] argv.[1]
+    GameJoltApi.authenticateUser() |> ignore
     let game = new TJoFGDGame.TJoFGDGame<Assets.AssetId, AssetType.AssetType<Texture2D,SpriteFont,SoundEffect>>(1000, 720, Assets.loadAssets, Update.updateGame, Render.drawGame, Microsoft.Xna.Framework.Color.Black)
     game.Run()
     0
